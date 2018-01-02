9e98b950e8978d8ecf12c1645ed025c24e26fa9a
mmedek <mmedek94@gmail.com>
2018-01-02 15:51:08 +0100

    Added evaluating models by entropy and perplexity

--- src/script.py
+++ src/script.py
@@ -1,8 +1,11 @@
+import math
+
 ################# classes for models
 class Model:
     
     def __init__(self):
         self.voc = {}
+        self.cnt = 0
         
     def build_voc(self, data):
         for i in range(len(data)):
@@ -12,6 +15,7 @@ class Model:
                     self.voc[sentence[j]] += 1
                 else:
                     self.voc[sentence[j]] = 1
+                self.cnt += 1   
      
     def get_occ(self, word):   
         if word in self.voc:
@@ -26,10 +30,28 @@ class ZerogramModel(Model):
         
     def get_prob(self, word):
         if word in self.voc:
-            return 1 / len(self.voc)
+            return 1 / self.cnt
         else:
             return 0
-
+        
+    def predict(self, data):
+        counter = 0
+        tmp = 0
+        for i in range(len(data)):
+            sentence = data[i]
+            for j in  range(len(sentence)):
+                prob = self.get_prob(sentence[j])
+                if prob == 0:
+                    tmp += -math.inf
+                else:
+                    tmp += math.log(prob) / math.log(2) 
+                counter += 1
+        # entropy = prob of each token / number of tokens
+        entropy = -1 / counter * tmp
+        print ('Zerogram model entropy is %f' % entropy)
+        perplexity = math.pow(2, entropy)
+        print ('Zerogram model perplexity is %f' % perplexity)
+        
 class UnigramModel(Model):
     
     def fit(self, data):
@@ -37,15 +59,34 @@ class UnigramModel(Model):
         
     def get_prob(self, word):
         if word in self.voc:
-            return self.voc[word] / len(self.voc)
+            return self.voc[word] / self.cnt
         else:
             return 0    
+        
+    def predict(self, data):
+        counter = 0
+        tmp = 0
+        for i in range(len(data)):
+            sentence = data[i]
+            for j in  range(len(sentence)):
+                prob = self.get_prob(sentence[j])
+                if prob == 0:
+                    tmp += -math.inf
+                else:
+                    tmp += math.log(prob) / math.log(2) 
+                counter += 1
+        # entropy = prob of each token / number of tokens
+        entropy = -1 / counter * tmp
+        print ('Unigram model entropy is %f' % entropy)
+        perplexity = math.pow(2, entropy)
+        print ('Unigram model perplexity is %f' % perplexity)
         
 class BigramModel(Model):
     
     def __init__(self):
         self.voc = {}
         self.bi_voc = {}
+        self.cnt = 0
         
     def build_bigram_voc(self, data):
         for i in range(len(data)):
@@ -66,9 +107,37 @@ class BigramModel(Model):
         if temp in self.bi_voc and word1 in self.voc:
             return self.bi_voc[temp] / self.voc[word1]
         else:
-            return 0      
+            return 0    
+     
+    def predict(self, data):
+        counter = 0
+        tmp = 0
+        for i in range(len(data)):
+            sentence = data[i]
+            for j in range(len(sentence) - 1):
+                prob = self.get_prob(sentence[j], sentence[j + 1])
+                if prob == 0:
+                    tmp += -math.inf
+                else:
+                    tmp += math.log(prob,2) 
+                counter += 1
+        # entropy = prob of each token / number of tokens
+        entropy = -1 / counter * tmp
+        print ('Bigram model entropy is %f' % entropy)
+        # perplexity 2^entropy
+        perplexity = math.pow(2, entropy)
+        print ('Bigram model perplexity is %f' % perplexity)
+        
 ################# functions
 
+def create_bigrams(data):
+    bigrams = []
+    for i in range(len(data)):
+        sentence = data[i]
+        for j in range(len(sentence) - 1):
+            bigrams = sentence[j] + CONCAT_SYM + sentence[j + 1]
+    return bigrams
+                
 # loads txt file
 def load_file(filename):
     sentences = []
@@ -126,3 +195,11 @@ bigram_model = BigramModel()
 bigram_model.fit(train_data)
 prob = bigram_model.get_prob('Robert', 'Lang')
 print('The probability of words \'Robert Lang\' in Bigram language model is %f' %prob)
+
+# test_bigrams = create_bigrams(test_data) - the entropy and perplexity will be
+# Infinity if we will find unseen sample according to equation for entropy
+zerogram_model.predict(test_data)
+unigram_model.predict(test_data)
+bigram_model.predict(test_data)
+
+# next we will use smoothing for the posibility evaluate unseen samples
